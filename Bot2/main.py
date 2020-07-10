ffa34a9cffdd8f305327fe5b3f084083ccbf30c2
Gustavo William <62532880+GustavoKristoffersen@users.noreply.github.com>
2020-07-10 14:37:01 -0300

    Update main.py

--- Bot2/main.py
+++ Bot2/main.py
@@ -1,10 +1,10 @@
 import tweepy
 import time
 
-consumer_key = "qFhbTmFTYVIoX3I4DViQJI85j"
-consumer_secret = "qrt8vNood2fyCiHXrAmsZcvnoZaMRhz0Nqt52RS25G08eZphQM"
-access_token = "1244052257257201668-0NY8vactaeJhO6QeCDkfVcoSenBXNe"
-access_token_secret = "3KnF9dRGjHthFrzcw4hkbwaPqOdHmDfsrVnz9GTjP5YWf"
+consumer_key = "HERE"
+consumer_secret = "HERE"
+access_token = "HERE"
+access_token_secret = "HERE"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
