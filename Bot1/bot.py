d72915949b7aa3b8703fd6d596cd4ff32ca3f405
Gustavo William <gustavowilliam0980@gmail.com>
2020-12-19 01:49:10 -0300

    Inserção do json de emojis

--- Bot1/bot.py
+++ Bot1/bot.py
@@ -1,10 +1,10 @@
 from TwitterAPI import TwitterAPI
 import tweepy
 
-keys = dict(consumer_key='jtHg4Sfdk3D3b0a4Ub8TqTrc2',
-          consumer_secret='qSwc34rMmAD2c1EYXIZXU8tA5UyvEPr8Ln6cQON6LwJnLlxG6W',
-          access_token_key='1241503413281718272-ScNoFrdQ9XeRnbRMc7PaddqDFofU6j',
-          access_token_secret='DJQo0DqzqDL04B929xGvSa06f1UupgfavpQqqqaYMfT9J')
+keys = dict(consumer_key='',
+          consumer_secret='',
+          access_token_key='',
+          access_token_secret='')
 
 api = TwitterAPI(**keys)
 
