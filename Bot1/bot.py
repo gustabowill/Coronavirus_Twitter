135bd5703ab55882464e68233a34ee18287affa2
Gustavo William <gustavowilliam0980@gmail.com>
2021-04-20 12:35:06 -0300

    configuração da api

--- Bot1/bot.py
+++ Bot1/bot.py
@@ -1,5 +1,4 @@
 from TwitterAPI import TwitterAPI
-import tweepy
 
 keys = dict(consumer_key='',
           consumer_secret='',
@@ -7,23 +6,3 @@ keys = dict(consumer_key='',
           access_token_secret='')
 
 api = TwitterAPI(**keys)
-
-def login_to_twitter(consumer_key, consumer_secret, access_token, access_token_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    api = tweepy.API(auth)
-    ret = {}
-    ret['api'] = api
-    ret['path'] = auth
-    return api
-
-def post_tweets(message):
-    consumer_key = ""
-    consumer_secret = ""
-    access_token = ""
-    access_token_secret = ""
-
-    api = login_to_twitter(consumer_key, consumer_secret, access_token, access_token_secret)
-
-    ret = api.update_status(status=message)
